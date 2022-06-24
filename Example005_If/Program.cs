7a44f4d5c58ea729e186d5afe42ecfbbda0cf719
Maksim <bagzmax75@gmail.com>
2022-06-24 11:34:02 +0300

    add Example005 If

---
+++ Example005_If/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "максим")
+{
+    Console.WriteLine("Да это-же Максим!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}

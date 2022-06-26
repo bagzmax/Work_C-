4f4180b404696a74f6eded39f06bb5722059b91e
Maksim <bagzmax75@gmail.com>
2022-06-26 17:30:41 +0300

    add Example 011 Metod Array

---
+++ Example011_MetodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {15, 22, 36, 41, 507, 68, 74, 80, 36};
+
+int n = array.Length; // n присваивается размерность массива
+int find = 36;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break; // прерви, отсанови
+    }
+    index++; // index = index + 1
+}
